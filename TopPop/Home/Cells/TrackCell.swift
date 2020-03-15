b7dcbf8482f2341d44ebdcc5bdffd4a57c811fb6
Petar Ivanković Milošević <pero.milosev@yahoo.com>
2020-03-15 20:32:18 +0100

    Making slow progress

---
+++ TopPop/Home/Cells/TrackCell.swift
@@ -0,0 +1,16 @@
+//
+//  TrackCell.swift
+//  TopPop
+//
+//  Created by five on 15/03/2020.
+//  Copyright © 2020 Pero. All rights reserved.
+//
+
+import UIKit
+
+class TrackCell: UITableViewCell {
+    
+    override func prepareForReuse() {
+           super.prepareForReuse()
+          }
+}
